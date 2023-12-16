36709f7167023c0b6d3bb6d9dc83fad89c64fd05
karthik19881025 <karthik25101988@outlook.com>
2023-12-16 22:41:34 +0530

    Learning Python Basics

---
+++ lang_fundamentals/06_input_and_output_statements/54_input_vs_raw_input_function.py
@@ -0,0 +1,6 @@
+"""
+raw_input - we have to do the type casting manually in the code. It can be used only in python 2 input - we don't
+need to do the type casting.  This function itself will take care of it.  It can be used both in python 2.x and
+python 3.x
+
+"""
